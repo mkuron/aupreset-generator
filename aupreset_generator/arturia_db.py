ee806dd34e70b3445945d78d24e29fd3b64bba68
Michael Kuron <m.kuron@gmx.de>
2022-08-16 23:00:00 +0200

    don't use subtypes for organ

--- aupreset_generator/arturia_db.py
+++ aupreset_generator/arturia_db.py
@@ -18,14 +18,17 @@ def convert_arturia_db():
 
     db = sqlite3.connect(f'file:{src}?mode=ro', uri=True)
     cur = db.cursor()
-    q = cur.execute("""SELECT DISTINCT Instruments.name, Types_V2.name
+    q = cur.execute("""SELECT DISTINCT Instruments.name, Types_V2.name, Subtypes_V2.name
                        FROM Preset_Id
                        INNER JOIN Instruments ON instrument_key = Instruments.key_id
                        INNER JOIN Types_V2 ON type_v2 = Types_V2.key_id
+                       INNER JOIN Subtypes_V2 ON subtype_v2 = Subtypes_V2.key_id
                        WHERE Types_V2.name NOT LIKE 'Template%'""")
     types = defaultdict(set)
+    subtypes = defaultdict(set)
     for row in q:
         types[row[0]].add(row[1])
+        subtypes[row[0]].add(row[2])
     q = cur.execute("""SELECT Instruments.name, Types_V2.name, Packs.name, Preset_Id.name, file_path, comment, Subtypes_V2.name
                        FROM Preset_Id
                        INNER JOIN Instruments ON instrument_key = Instruments.key_id
@@ -41,13 +44,13 @@ def convert_arturia_db():
             continue
         with open(plugin, 'rb') as f:
             magic = int(plistlib.load(f)['AudioComponents'][0]['subtype'].encode('ascii').hex(), base=16)
-        category = row[1].replace(' & ', ' and ') if len(types[instrument]) > 2 else row[6]
+        category = row[1].replace(' & ', ' and ') if len(types[row[0]]) > 2 else row[6]
+        category = os.path.join(row[2], category) if len(subtypes[row[0]]) >= 2 else row[2]
         name = row[3]
         if row[2] == 'Vintage Factory' and row[5]:
             m = re.search("Preset ([0-9]+) from the original factory library", row[5])
             if m:
                 name = f'{m.group(1)} {name}'
-        category = os.path.join(row[2], category)
         nksf = {'NISI': {'vendor': 'Arturia', 'name': name}, 'PLID': {'VST.magic': magic}}
         dest = os.path.join(destdir, instrument, category, name) + '.aupreset'
         print(row, dest)
